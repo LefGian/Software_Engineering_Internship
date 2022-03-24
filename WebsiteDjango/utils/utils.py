942729a0f5d16a5ef654328ca997202fcb695933
tstempel <tstempel@students.uni-mainz.de>
2022-03-24 13:34:43 +0100

    website changes von Gianluca, theo

--- WebsiteDjango/utils/utils.py
+++ WebsiteDjango/utils/utils.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User, Group
+from .forms import Aufgabe
 # from ... import Aufgabe muss noch importiert werden
 
 
@@ -13,9 +14,9 @@ def get_group(user: User):
 
     return group_names
 
-def update_user(user: User, first_name: str, last_name: str, new_password: str):
+def update_user(user: User, username: str, first_name: str, last_name: str, new_password: str):
+    user.username = username
     user.first_name = first_name
     user.last_name = last_name
     user.set_password(new_password)
-
     user.save()
