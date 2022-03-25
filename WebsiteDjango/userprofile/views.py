06765a699b871dcdfb6ccde9e9e1947e842b8b14
tstempel <tstempel@students.uni-mainz.de>
2022-03-25 14:06:14 +0100

    changes

--- WebsiteDjango/userprofile/views.py
+++ WebsiteDjango/userprofile/views.py
@@ -16,7 +16,6 @@ def userprofile(request):
 
     if request.method == 'POST':
 
-<<<<<<< HEAD
         password = request.POST['jgu-password']
         user_update_dict = {
             'csrfmiddlewaretoken': request.POST['csrfmiddlewaretoken'],
@@ -24,24 +23,6 @@ def userprofile(request):
             'last_name': request.POST['jgu-nachname'],
             'username': request.POST['jgu-username'],
         }
-=======
-        password = ''
-        password1 = ''
-        password2 = ''
-        if 'jgu-password' in request.POST:
-            password = request.POST['jgu-password']
-        if 'jgu-password-new' in request.POST:
-            password1 = request.POST['jgu-password-new']
-        if 'jgu-password-new-confirm' in request.POST:
-            password2 = request.POST['jgu-password-new-confirm']
-
-        user_update_form_dict = {'csrfmiddlewaretoken': request.POST['csrfmiddlewaretoken'],
-                                 'first_name': request.POST['jgu-vorname'],
-                                 'last_name': request.POST['jgu-nachname'],
-                                 'username': request.POST['jgu-username'],
-                                 }
-
->>>>>>> main
         user_check = authenticate(request, username=user.username, password=password)
 
         if user_check is not None:
@@ -71,7 +52,6 @@ def userprofile(request):
                     user_update_form.save()
                     update_session_auth_hash(request, user)
                     return redirect('userprofile-userprofile')
-<<<<<<< HEAD
             elif user_update_form.is_valid():
                 user_update_form.save()
                 return redirect('userprofile-userprofile')
@@ -98,46 +78,5 @@ def userprofile(request):
         'error_messages' : error_messages,
         'show_error' : show_error,
         'user_role' : 'Student',
-=======
-
-
-
-
-
-        else:
-            fehlermeldung_password = 'Password Wrong'
-
-        if form_user_update.is_valid():
-            form_user_update.save()
-
-            return redirect('userprofile-userprofile')
-
-    fehlermeldung_user_update = form_user_update.errors.as_data()
-    fehlermeldung_password_form = form_user_pwd.errors.as_data()
-    print(fehlermeldung_user_update)
-    if fehlermeldung_password:
-        fehlermeldung_user_update_str_arr = [fehlermeldung_password]
-
-    for key in fehlermeldung_user_update:
-        for error_arr in fehlermeldung_user_update[key]:
-            fehlermeldung_user_update_str_arr += error_arr
-
-    for key in fehlermeldung_password_form:
-        for error_arr in fehlermeldung_password_form[key]:
-            fehlermeldung_user_update_str_arr += error_arr
-
-    print(fehlermeldung_user_update_str_arr)
-    user_group = 'filler'
-    show_error = 0
-    if fehlermeldung_user_update_str_arr:
-        show_error = 1
-
-    context = {
-        'form_user_update': form_user_update,
-        'user': user,
-        'user_group': user_group,
-        'fehlermeldung_user_update_str_arr': fehlermeldung_user_update_str_arr,
-        'show_error': show_error,
->>>>>>> main
     }
     return render(request, 'userprofile/nutzerprofil.html', context)
