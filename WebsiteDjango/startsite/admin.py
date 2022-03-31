f328b4c062fc48604b86ad0e82982fb08e72e947
tstempel <tstempel@students.uni-mainz.de>
2022-03-31 16:12:50 +0200

    add documentation stuff

--- WebsiteDjango/startsite/admin.py
+++ WebsiteDjango/startsite/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Aufgabe, Fachgebiet, Themengebiet
 
-# Register your models here.
+# Register your models here. So that they are shown in the admin page
 
 admin.site.register(Aufgabe)
 admin.site.register(Fachgebiet)
--- WebsiteDjango/startsite/forms.py
+++ WebsiteDjango/startsite/forms.py
@@ -4,6 +4,11 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
+    """Form used to Register a User wich is stored in User-DB-Model
+
+    :param UserCreationForm: Inherits from the ModelForm from Django
+    :type UserCreationForm: form
+    """
     class Meta:
         model = User    # define with wich model to interact
         # specify form fields that are displayed, and order of them
